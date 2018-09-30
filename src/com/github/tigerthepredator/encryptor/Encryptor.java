f36d2e00640029b928a4d031bcfa30dc126fcc35
N <TigerThePredator>
2018-09-30 16:27:56 -0400

    Switched to GCM mode.

--- src/com/github/tigerthepredator/encryptor/Encryptor.java
+++ src/com/github/tigerthepredator/encryptor/Encryptor.java
@@ -12,6 +12,7 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -23,13 +24,13 @@ public class Encryptor {
             SecretKeySpec keySpec = new SecretKeySpec(sha256(password), "AES");
 
             // Create an AES CBC cipher
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            Cipher cipher = Cipher.getInstance("AES/GCM/PKCS5Padding");
 
             // Generate a random initialization vector
             SecureRandom randomSecureRandom = SecureRandom.getInstance("SHA1PRNG");
             byte[] iv = new byte[cipher.getBlockSize()];
             randomSecureRandom.nextBytes(iv);
-            IvParameterSpec ivParams = new IvParameterSpec(iv);
+            GCMParameterSpec ivParams = new GCMParameterSpec(128, iv);
 
             // Initialize the cipher in encrypt mode using the given key and IV
             cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParams);
@@ -51,14 +52,14 @@ public class Encryptor {
             SecretKeySpec keySpec = new SecretKeySpec(sha256(password), "AES");
 
             // Create an AES CBC cipher
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            Cipher cipher = Cipher.getInstance("AES/GCM/PKCS5Padding");
 
             // Separate the ciphertext from the IV (IV should end in two equal signs)
             String iv = ciphertext.split("==")[0] + "==";
             String toDecrypt = ciphertext.substring(iv.length());
 
             // Generate the IV params object from the IV string
-            IvParameterSpec ivParams = new IvParameterSpec(Base64.getDecoder().decode(iv));
+            GCMParameterSpec ivParams = new GCMParameterSpec(128, Base64.getDecoder().decode(iv));
 
             // Initialize the cipher in decrypt mode using the given key and IV
             cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParams);
